5423c0885de55697659118c3c8e162d75cd6c13c
Richard Pham <38672776+dungtn911@users.noreply.github.com>
2020-06-17 22:15:54 -0700

    Create w3_searchBST.py

---
+++ june-challenge/w3_searchBST.py
@@ -0,0 +1,33 @@
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+      2     
+     / \   
+    1   3
+    
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+       
+        if root:
+            if root.val == val:
+                return root
+            elif root.val < val:
+                return self.searchBST(root.right, val)
+            else:
+                return self.searchBST(root.left, val)
